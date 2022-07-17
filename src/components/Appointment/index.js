7d9b1c6501cf254c6fd6f719fce9b97629e43f15
Aidan Prior <sifiwizard.code@gmail.com>
2022-07-17 13:33:41 -0600

    Days axios

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,10 +1,17 @@
 import React from "react";
 import "./styles.scss";
+import Header from "./Header";
+import Show from "./Show";
+import Empty from "./Empty";
+
 
 export default function Appointment(props) {
+  const appointment = props.interview ? <Show student={props.interview.student} interviewer={props.interview.interviewer} /> : <Empty/>
+
   return (
    <article className="appointment">
-
+     <Header time={props.time}/>
+     {appointment}
    </article>
   );
 }
